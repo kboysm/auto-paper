94348d9cab81e73af9e9fa2c64866208364d7053
kboysm <lishal12@naver.com>
2021-04-19 07:27:06 +0900

    배포서버 login test 및 404 오류 해결

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,4 @@
 <template>
-  <div id="nav">
-    {{init}}
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
   <router-view/>
 </template>
 <script>
