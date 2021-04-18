44bcdadd3492d2a2f3e67b9db99b65e6488c929a
kboysm <lishal12@naver.com>
2021-04-19 01:43:25 +0900

    firebase git actions .env test

--- src/main.js
+++ src/main.js
@@ -2,5 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import {firebaseInstance} from './plugins/fbase'
+console.log(firebaseInstance)
 
 createApp(App).use(store).use(router).mount('#app')
