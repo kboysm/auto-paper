2b62c873a73ba26b067caf49f807d6febf7089a5
kboysm <lishal12@naver.com>
2021-04-23 12:47:35 +0900

    배포 threeJs 테스트

--- src/views/Auth.vue
+++ src/views/Auth.vue
@@ -1,14 +1,39 @@
 <template>
     <SignForm />
+    <div></div>
 </template>
 <script>
 import SignForm from '../components/SignForm'
+import * as THREE from 'three'
+import { onMounted } from 'vue'
 export default {
     components:{
         SignForm
     },
     setup() {
-        
+        const init = () => {
+            let scene = new THREE.Scene();
+            let camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight);
+            let renderer = new THREE.WebGLRenderer({ antialias: true })
+            renderer.setSize( window.innerWidth, window.innerHeight )
+            document.body.append(renderer.domElement);
+
+            let geometry = new THREE.BoxGeometry( 1, 1, 1 )
+            let material = new THREE.MeshBasicMaterial( {color: 0xff0000})
+            let cube = new THREE.Mesh( geometry, material)
+            scene.add( cube )
+
+            cube.position.z = -5;
+            let animate = () => {
+                cube.rotation.x += 0.01;
+                renderer.render(scene, camera);
+                requestAnimationFrame( animate )
+            }
+            animate()
+        }
+        onMounted(()=> {
+            init()
+        })
     },
 }
 </script>
