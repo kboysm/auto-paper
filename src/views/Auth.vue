eb8091de6b49c88d0c21512292ba716a26386b44
kboysm <lishal12@naver.com>
2021-04-25 17:06:54 +0900

    particleJs 적용

--- src/views/Auth.vue
+++ src/views/Auth.vue
@@ -1,106 +1,26 @@
 <template>
-    <!-- <div class="bg-three"></div> -->
     <Particles
         class="bg-three"
         id="tsparticles"
-        :particlesInit="particlesInit"
-        :particlesLoaded="particlesLoaded"
-        :options="{
-            background: {
-                color: {
-                    value: '#0d47a1'
-                }
-            },
-            fpsLimit: 60,
-            interactivity: {
-                detectsOn: 'canvas',
-                events: {
-                    onClick: {
-                        enable: true,
-                        mode: 'push'
-                    },
-                    onHover: {
-                        enable: true,
-                        mode: 'repulse'
-                    },
-                    resize: true
-                },
-                modes: {
-                    bubble: {
-                        distance: 400,
-                        duration: 2,
-                        opacity: 0.8,
-                        size: 40
-                    },
-                    push: {
-                        quantity: 4
-                    },
-                    repulse: {
-                        distance: 200,
-                        duration: 0.4
-                    }
-                }
-            },
-            particles: {
-                color: {
-                    value: '#ffffff'
-                },
-                links: {
-                    color: '#ffffff',
-                    distance: 150,
-                    enable: true,
-                    opacity: 0.5,
-                    width: 1
-                },
-                collisions: {
-                    enable: true
-                },
-                move: {
-                    direction: 'none',
-                    enable: true,
-                    outMode: 'bounce',
-                    random: false,
-                    speed: 6,
-                    straight: false
-                },
-                number: {
-                    density: {
-                        enable: true,
-                        value_area: 800
-                    },
-                    value: 80
-                },
-                opacity: {
-                    value: 0.5
-                },
-                shape: {
-                    type: 'circle'
-                },
-                size: {
-                    random: true,
-                    value: 5
-                }
-            },
-            detectRetina: true
-        }"
-        />
+        :options="particlesOption"
+    />
     <SignForm />
 </template>
 <script>
 import SignForm from '../components/SignForm'
 // import getBackgound from '../composables/getBackgound'
-
 import { onMounted, onBeforeUnmount, ref } from 'vue'
 export default {
     components:{
         SignForm
     },
     setup() {
-        // const { animateId, rendererDom } = getBackgound();
-		onBeforeUnmount(()=> {
-			// cancelAnimationFrame( animateId )
-			// document.body.removeChild(rendererDom)
-		})
+        const particlesOption= require('./particlesConfig').particlesOption
+        onMounted(()=>{
+        })
+        return{
+            particlesOption
+        }
     },
 }
 </script>
