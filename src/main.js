f01042557ff7151bdd921ada66eda9b18762b7b9
kboysm <lishal12@naver.com>
2021-04-29 10:19:48 +0900

    데이터 불러오기 거의 완성 테스트 배포

--- src/main.js
+++ src/main.js
@@ -3,5 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import Particles from "particles.vue3";
+import VueScrollingTable from "vue-scrolling-table"
+import "vue-scrolling-table/dist/style.css"
 
-createApp(App).use(store).use(router).use(Particles).mount('#app')
+createApp(App).use(store).use(router).use(Particles).component(VueScrollingTable.name, VueScrollingTable).mount('#app')
--- src/views/ImportData.vue
+++ src/views/ImportData.vue
@@ -1,3 +1,122 @@
 <template>
-    ImportData
+    <div class="if-data-false">
+        <div id="drop" @drop="handleDrop" @dragover="handleDragover" @dragenter="handleDragover">Drop Here</div>
+    </div>
+    <div class="row">
+        <vue-scrolling-table>
+            <template #thead>
+            <tr>
+                <th v-for="(col,idx) in headers"
+                class="tableOption"
+                :key="idx">{{ col }}</th>
+            </tr>
+            </template>
+            <template #tbody>
+            <tr v-for=" (item,idx) in tickets" :key="idx">
+                <td v-for=" (col ,idx) in item"
+                class="tableOption"
+                :key="idx">{{ col }}</td>
+            </tr>
+            </template>
+            </vue-scrolling-table>
+        </div>
 </template>
+<script>
+import { reactive } from 'vue'
+import XLSX from 'xlsx'
+
+export default {
+    
+    setup(){
+        let tickets = reactive([])
+        let headers = reactive([])
+
+        const handleDrop = ( e ) => {
+            e.stopPropagation();
+            e.preventDefault();
+            console.log("DROPPED");
+            let files = e.dataTransfer.files, i, f;
+            for (i = 0, f = files[i]; i != files.length; ++i) {
+                let reader = new FileReader(),
+                name = f.name;
+                reader.onload = function(e) {
+                let results, 
+                    data = e.target.result, 
+                    fixedData = fixdata(data), 
+                    workbook=XLSX.read(btoa(fixedData), {type: 'base64'}), 
+                    firstSheetName = workbook.SheetNames[0], 
+                    worksheet = workbook.Sheets[firstSheetName];
+                get_header_row(worksheet).forEach( item => {
+                    headers.push(item)
+                });
+                results=XLSX.utils.sheet_to_json(worksheet);
+                results.forEach( item => {
+                    tickets.push( item )
+                });
+                };
+                reader.readAsArrayBuffer(f);
+            }
+        }
+        const handleDragover = ( e ) => {
+            e.stopPropagation();
+            e.preventDefault();
+            e.dataTransfer.dropEffect = 'copy';
+        }
+        function get_header_row(sheet) {
+            var headers = [], range = XLSX.utils.decode_range(sheet['!ref']);
+            var C, R = range.s.r; /* start in the first row */
+            for(C = range.s.c; C <= range.e.c; ++C) { /* walk every column in the range */
+                var cell = sheet[XLSX.utils.encode_cell({c:C, r:R})] /* find the cell in the first row */
+                var hdr = "UNKNOWN " + C; // <-- replace with your desired default 
+                if(cell && cell.t) hdr = XLSX.utils.format_cell(cell);
+                headers.push(hdr);
+            }
+            return headers;
+        }
+        function fixdata(data) {
+            var o = "", l = 0, w = 10240;
+            for(; l<data.byteLength/w; ++l) o+=String.fromCharCode.apply(null,new Uint8Array(data.slice(l*w,l*w+w)));
+            o+=String.fromCharCode.apply(null, new Uint8Array(data.slice(l*w)));
+            return o;
+        }
+        
+        return {
+            tickets,
+            headers,
+            handleDrop,
+            handleDragover
+        }
+    }
+}
+</script>
+<style lang="scss" scoped>
+* {
+    box-sizing: border-box;
+    -moz-box-sizing: border-box;
+}
+    .if-data-false {
+        width: 210mm;
+        min-height: 50mm;
+        padding: 20mm;
+        margin: 10mm auto;
+        border: 1px #D3D3D3 solid;
+        border-radius: 5px;
+        background: white;
+        box-shadow: 0 0 5px rgba(0, 0, 0, 0.1);
+        color: black;
+        opacity: .95;
+    }
+    #drop{
+        border: 2px dashed #bbb;
+        -moz-border-radius: 5px;
+        -webkit-border-radius: 5px;
+        border-radius: 5px;
+        padding: 25px;
+        text-align: center;
+        font: 20pt bold,"Vollkorn";
+        color: #bbb;
+    }
+    .tableOption {
+        color: black;
+    }
+</style>
