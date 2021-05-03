8c03c1b3c909a79103e4aa331c9bf39cde4ec906
kboysm <lishal12@naver.com>
2021-05-04 02:54:49 +0900

    서류생성 layout 작업 중 각 컴포넌트 배치

--- src/views/CreatePaperForm.vue
+++ src/views/CreatePaperForm.vue
@@ -1,3 +1,62 @@
 <template>
-    createPaperForm
+    <div class="create-paper-container">
+        <div class="options-box">
+            option- box
+        </div>
+        <div class="create-paper page">
+            <div class="subpage" id="content">
+                createPaper
+            </div>
+        </div>
+        <div class="preview-paper page">
+            <div class="subpage" id="content">
+                previewPaper
+            </div>
+        </div>
+    </div>
 </template>
+<style lang="scss" scoped>
+ create-paper-container {
+            margin: 0;
+            padding: 0;
+            font: 12pt "Tahoma";
+        }
+        * {
+            box-sizing: border-box;
+            -moz-box-sizing: border-box;
+        }
+        .page {
+            display: inline-block;
+            margin: 1cm;
+            width: 21cm;
+            min-height: 29.7cm;
+            padding: 2cm;
+            // margin: 1cm auto;
+            border-radius: 5px;
+            background: white;
+            box-shadow: 0 0 5px rgba(0, 0, 0, 0.1);
+        }
+        .subpage {
+            padding: 1cm;
+            height: 256mm;
+            color: black;
+        }
+        @page {
+            size: A4 landscape;
+            margin: 0;
+            /*size: landscape;*/
+        }
+        @media print {
+            .page {
+                margin: 0;
+                border: initial;
+                border-radius: initial;
+                width: initial;
+                min-height: initial;
+                box-shadow: initial;
+                background: initial;
+                page-break-after: always;
+            }
+        }
+
+</style>
